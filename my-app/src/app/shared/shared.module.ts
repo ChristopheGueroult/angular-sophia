4f2d17d6a20765d10e3f033cca0c6585ceadb928
christopheGueroult <christophegueroult@gmail.com>
2018-05-22 15:51:45 +0200

    bootstrap and nav

--- my-app/src/app/shared/shared.module.ts
+++ my-app/src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [NavComponent],
+  exports: [NavComponent]
 })
 export class SharedModule { }
