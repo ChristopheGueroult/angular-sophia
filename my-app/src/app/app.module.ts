465f5a8f62d9c4a484c5bb04bf030f5c4c25c28b
christopheGueroult <christophegueroult@gmail.com>
2018-05-22 15:00:01 +0200

    modules de l'application

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { HomeModule } from './home/home.module';
+import { PageNotFoundModule } from './page-not-found/page-not-found.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule,
+    SharedModule,
+    HomeModule,
+    PageNotFoundModule
   ],
   providers: [],
   bootstrap: [AppComponent]
